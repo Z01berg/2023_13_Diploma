09f8dc72750b5c82a3ad534ee45fd1b8d8e33c80
s22714 <s22714@pjwstk.edu.pl>
2023-12-28 15:41:18 +0100

    inventory

---
+++ Assets/Scripts/Equipment/Inventory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static Inventory Instance;
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        Instance = this;
+    }
+
+    #endregion
+
+    public List<Item> items = new List<Item>();
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+}
