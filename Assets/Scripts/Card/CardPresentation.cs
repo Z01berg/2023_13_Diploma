c24f980e8489c1b2664ccbd93b418e30db2015d7
maXo <maks.janas@gmail.com>
2023-12-28 13:56:27 +0100

    tak

---
+++ Assets/Scripts/Card/CardPresentation.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPresentation : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        
+    }
+
+    private void OnMouseExit()
+    {
+        throw new NotImplementedException();
+    }
+}
