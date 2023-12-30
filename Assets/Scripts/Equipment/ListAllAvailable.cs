8d28abd2ffb5749f06f0a8164ad55ad4447dc415
s22714 <s22714@pjwstk.edu.pl>
2023-12-30 15:11:28 +0100

    item UI changes

--- Assets/Scripts/Equipment/ListAllAvailable.cs
+++ Assets/Scripts/Equipment/ListAllAvailable.cs
@@ -8,20 +8,26 @@ public class ListAllAvailable : MonoBehaviour
 {
 
     [SerializeField] private Transform parent;
-    [SerializeField] private GameObject prefab;
+    [SerializeField] private GameObject slotPrefab;
+    [SerializeField] private GameObject itemUIPrefab;
 
     void Start()
     {
         foreach(var x in Inventory.Instance.items)
         {
-            var item = Instantiate(prefab);
-            var img = item.GetComponentsInChildren<UnityEngine.UI.Image>();
-            foreach(var y in img)
-            {
-                if(y.gameObject.transform.parent != null)
-                    y.sprite = x.icon;
-            }
-            item.transform.SetParent(parent);   
+            var slot = Instantiate(slotPrefab);
+            slot.transform.SetParent(parent);
+
+            var item = Instantiate(itemUIPrefab);
+            item.transform.SetParent(slot.transform);
+            item.GetComponent<UnityEngine.UI.Image>().sprite = x.icon;
+            //var img = item.GetComponentsInChildren<UnityEngine.UI.Image>();
+            //foreach(var y in img)
+            //{
+            //    if(y.gameObject.transform.parent != null)
+            //        y.sprite = x.icon;
+            //}
+            
         }
     }
 
