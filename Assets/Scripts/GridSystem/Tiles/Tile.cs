e3cfdb064bd7bfa147efea4023df9a97d82e289f
Juliusz <s23751@pjwstk.edu.pl>
2024-01-06 16:06:49 +0100

    Tiles for generating grid

---
+++ Assets/Scripts/GridSystem/Tiles/Tile.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace GridSystem.Tiles
+{
+    public class Tile : MonoBehaviour
+    {
+
+        [SerializeField] private Color _baseColor, _offsetColor;
+        [SerializeField] private SpriteRenderer _renderer;
+        [SerializeField] private GameObject _highlight;
+
+        public void Init(bool isOffset)
+        {
+            _renderer.color = isOffset ? _offsetColor : _baseColor;
+        }
+
+        private void OnMouseEnter()
+        {
+            _highlight.SetActive(true);
+        }
+
+        private void OnMouseExit()
+        {
+            _highlight.SetActive(false);
+        }
+    }
+}
