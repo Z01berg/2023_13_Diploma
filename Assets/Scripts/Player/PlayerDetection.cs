80ab2acbc753709ca144bbbffb2681d8fb0a0755
Juliusz <s23751@pjwstk.edu.pl>
2024-01-23 10:49:21 +0100

    Player detection script (work in progress))

---
+++ Assets/Scripts/Player/PlayerDetection.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerDetection : MonoBehaviour
+    {
+
+        private Transform _playerTransform;
+        
+        
+        private void Start()
+        {
+            
+        }
+        
+        private void Update()
+        {
+            
+        }
+    }
+}
