e9682b8c449c7f06007fdbc757a939c33efeb1d4
s22714 <s22714@pjwstk.edu.pl>
2024-01-23 20:09:36 +0100

    manager stuff

--- Assets/Scripts/Equipment/DontDestroy.cs
+++ Assets/Scripts/Equipment/DontDestroy.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    public bool active = true;
     private void Awake()
     {
+        if (!active) return;
+
         GameObject[] objs = GameObject.FindGameObjectsWithTag(this.gameObject.tag);
 
         if (objs.Length > 1)
