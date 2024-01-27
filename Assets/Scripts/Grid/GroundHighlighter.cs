af16c85c1a5a587e8ecad3fe4e13bd051fde0547
Juliusz <s23751@pjwstk.edu.pl>
2024-01-27 16:26:58 +0100

    Update GroundHighlighter.cs

--- Assets/Scripts/Grid/GroundHighlighter.cs
+++ Assets/Scripts/Grid/GroundHighlighter.cs
@@ -6,34 +6,120 @@ namespace Grid
 {
     public class GroundHighlighter : MonoBehaviour
     {
-
-        private UnityEngine.Grid grid;
-        [SerializeField] private Tilemap interacativeTilemap;
+        private UnityEngine.Grid _grid;
+        [SerializeField] private Tilemap interactiveTilemap;
         [SerializeField] private Tile hoverTile;
+        [SerializeField] private Tile moveRangeTile;
+        [SerializeField] private Tile skillRangeTile;
 
-        private Vector3Int previousMousePosition = new Vector3Int();
+        private Vector3Int _previousMousePosition;
+        private HighlightMode _currentHighlightMode = HighlightMode.SingleTile;
+        private Transform _playerTransform;
+
+        private enum HighlightMode
+        {
+            MoveRange,
+            SkillRange,
+            SingleTile
+        }
 
         private void Start()
         {
-            grid = gameObject.GetComponent<UnityEngine.Grid>();
+            _grid = gameObject.GetComponent<UnityEngine.Grid>();
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         }
 
         private void Update()
         {
             Vector3Int mousePosition = GetMousePosition();
-            if (!mousePosition.Equals(previousMousePosition))
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                interacativeTilemap.SetTile(previousMousePosition, null);
-                interacativeTilemap.SetTile(mousePosition, hoverTile);
-                previousMousePosition = mousePosition;
+                _currentHighlightMode = HighlightMode.MoveRange;
             }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                _currentHighlightMode = HighlightMode.SkillRange;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                _currentHighlightMode = HighlightMode.SingleTile;
+            }
+
+            if (!mousePosition.Equals(_previousMousePosition))
+            {
+                ClearTiles();
+                HighlightTiles(mousePosition);
+                _previousMousePosition = mousePosition;
+            }
+        }
+
+        void ClearTiles()
+        {
+            interactiveTilemap.ClearAllTiles();
+        }
+
+        void HighlightTiles(Vector3Int mousePosition)
+        {
+            switch (_currentHighlightMode)
+            {
+                case HighlightMode.MoveRange:
+                    HighlightMoveRange(2);
+                    break;
+
+                case HighlightMode.SkillRange:
+                    HighlightSkillRange(mousePosition, 2);
+                    break;
+
+                case HighlightMode.SingleTile:
+                    interactiveTilemap.SetTile(mousePosition, hoverTile);
+                    break;
+            }
+        }
+
+        void HighlightMoveRange(int actionPoints)
+        {
+            Vector3Int playerPosition = GetPlayerPosition();
+
+            for (int x = -actionPoints; x <= actionPoints; x++)
+            {
+                for (int y = -actionPoints; y <= actionPoints; y++)
+                {
+                    int distance = Mathf.Abs(x) + Mathf.Abs(y);
+                    if (distance <= actionPoints)
+                    {
+                        Vector3Int tilePosition = new Vector3Int(playerPosition.x + x, playerPosition.y + y, playerPosition.z);
+                        interactiveTilemap.SetTile(tilePosition, moveRangeTile);
+                    }
+                }
+            }
+        }
+
+        void HighlightSkillRange(Vector3Int mousePosition, int skillRange)
+        {
+            for (int x = -skillRange; x <= skillRange; x++)
+            {
+                for (int y = -skillRange; y <= skillRange; y++)
+                {
+                    int distance = Mathf.Abs(x) + Mathf.Abs(y);
+                    if (distance <= skillRange)
+                    {
+                        Vector3Int tilePosition = new Vector3Int(mousePosition.x + x, mousePosition.y + y, mousePosition.z);
+                        interactiveTilemap.SetTile(tilePosition, skillRangeTile);
+                    }
+                }
+            }
+        }
+        
+        Vector3Int GetPlayerPosition()
+        {
+            return _grid.WorldToCell(_playerTransform.position);
         }
 
         Vector3Int GetMousePosition()
         {
             Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            return grid.WorldToCell(mouseWorldPosition);
+            return _grid.WorldToCell(mouseWorldPosition);
         }
-        
     }
 }
