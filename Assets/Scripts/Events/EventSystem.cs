9dab3b76e60af36f57a657dce3db22d15d5d16e6
Zahar Zubik <zaharzubik@gmail.com>
2024-02-25 20:45:31 +0100

    Custom_Brush + test tilemap

--- Assets/Scripts/Events/EventSystem.cs
+++ Assets/Scripts/Events/EventSystem.cs
@@ -4,5 +4,6 @@ using UnityEngine.Events;
 public static class EventSystem
 {
     public static UnityEvent<bool> PlayerMove = new UnityEvent<bool>();
+    public static UnityEvent<bool> EnemyMove = new UnityEvent<bool>();
     public static UnityEvent<GameObject> WhatHP = new UnityEvent<GameObject>();
 }
