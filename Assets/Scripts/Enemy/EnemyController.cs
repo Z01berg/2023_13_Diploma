cc9cf5689e6a3d992f7492026e4c03b019f7fd5b
Juliusz <s23751@pjwstk.edu.pl>
2024-03-27 16:51:12 +0100

    Update: Intergrating pathfinding system; EnemyController.cs

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -28,14 +30,22 @@ using UnityEngine.Tilemaps;
         //defaultowo 1f, ale można zmienić w zależności od potrzeb
         [SerializeField] private float _tileSize = 1f;
         [SerializeField] private int _healthPoints;
+        [SerializeField] private float _moveSpeed = 5f;
 
         private Transform _player;
         private Tilemap _gridOverlayTilemap;
         private bool _isCombatStarted;
+        private Pathfinding _pathfinding;
 
         void Start()
         {
             _player = GameObject.FindGameObjectWithTag("Player").transform;
+
+            _pathfinding = GetComponent<Pathfinding>();
+            if (_pathfinding == null)
+            {
+                Debug.LogError("Pathfinding cript not found on the Enemy object");
+            }
 
             GameObject gridOverlayObject = GameObject.FindWithTag("GridOverlayTag");
             if (gridOverlayObject != null)
@@ -76,13 +86,52 @@ using UnityEngine.Tilemaps;
         bool EnterCombat()
         {
                 _gridOverlayTilemap.GetComponent<Renderer>().enabled = true;
-                return _isCombatStarted = true;
+                _isCombatStarted = true;
+                ChasePlayer();
+                return _isCombatStarted;
         }
 
         bool ExitCombat()
         {
                 _gridOverlayTilemap.GetComponent<Renderer>().enabled = false;
                 return _isCombatStarted = false;
+        }
+        
+        void ChasePlayer()
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 targetPosition = _player.position;
+
+            List<Vector3> path = _pathfinding.FindPath(startPosition, targetPosition);
+
+            if (path != null && path.Count > 0)
+            {
+                Vector3 nextWaypoint = path[0];
+                Vector3 currentPosition = new Vector3(
+                    Mathf.Round(transform.position.x),
+                    Mathf.Round(transform.position.y),
+                    0f);
+                Vector3 targetTileCenter = new Vector3(
+                    Mathf.Round(nextWaypoint.x) + 0.5f,
+                    Mathf.Round(nextWaypoint.y) + 0.5f,
+                    0f);
+
+                if (currentPosition == targetTileCenter)
+                {
+                    path.RemoveAt(0);
+                    if (path.Count > 0)
+                    {
+                        nextWaypoint = path[0];
+                        targetTileCenter = new Vector3(
+                            Mathf.Round(nextWaypoint.x) + 0.5f,
+                            Mathf.Round(nextWaypoint.y) + 0.5f,
+                            0f);
+                    }
+                }
+                
+                float step = _moveSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, targetTileCenter, step);
+            }
         }
 
         public bool IsCombatStarted => _isCombatStarted;
@@ -93,13 +142,13 @@ using UnityEngine.Tilemaps;
             set => _healthPoints = value;
         }
         
-        /*
+        
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireCube(transform.position, new Vector3(_visionRangeInTiles * _tileSize * 2, _visionRangeInTiles * _tileSize * 2, 0));
         }
-        */
+        
         
         
     }
