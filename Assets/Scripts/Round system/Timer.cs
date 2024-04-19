7fc47e8370563bd8295e92fce8e0a679199844f0
Z01berg <zaharzubik@gmail.com>
2024-04-19 22:48:25 +0200

    Add: Coroutine fix; Half Timer limits

--- Assets/Scripts/Round system/Timer.cs
+++ Assets/Scripts/Round system/Timer.cs
@@ -33,7 +33,7 @@ public class Timer : MonoBehaviour
     private int _activeTimerIndex = 0; //czyja runda
     private bool _counting = false; // po wciśnięciu enter
     
-    private float _timeToPause = 1f; //do animacji timerów
+    private float _timeToPause = 0.5f; //do animacji timerów
     
     private bool _cheat = false; // włączenie na "R CTRL" zmieniania znaczenia timerów
 
@@ -135,7 +135,7 @@ public class Timer : MonoBehaviour
                 }
                 
                 _turn.text = "Turn: " + _timers[_activeTimerIndex].Tag;
-                _timeToPause = 1f;
+                _timeToPause = 0.5f;
             }
         }
 
@@ -157,13 +157,13 @@ public class Timer : MonoBehaviour
 
     IEnumerator AnimationPauseCoroutine()
     {
-        if (_timeToPause > 0.1)
+        if (_timeToPause > 0.01)
         {
-            _timeToPause -= 0.5f;
+            _timeToPause -= 0.05f;
         }
         else
         {
-            _timeToPause = 0.1f;
+            _timeToPause = 0.01f;
         }
 
         yield return new WaitForSeconds(_timeToPause);
@@ -178,9 +178,24 @@ public class Timer : MonoBehaviour
             if (i == _activeTimerIndex)
             {
                 _timers[i].Value += change;
+                KeepTimerInsideBreakets(_timers[i].Value);
             }
         }
         UpdateTexts();
+    }
+    
+    int KeepTimerInsideBreakets(int timerValue)
+    {
+        if (timerValue > 99)
+        {
+            timerValue = 99;
+        }
+        else if (timerValue < 0)
+        {
+            timerValue = 0;
+        }
+        
+        return timerValue;
     }
     
     void ChangeActiveTimer(int change)
