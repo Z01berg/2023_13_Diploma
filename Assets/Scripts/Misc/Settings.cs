685c529f88fe64ac814cc86edb1ff509388927b0
Z01berg <zaharzubik@gmail.com>
2024-05-02 13:50:50 +0200

    Add: small modifayers for old code

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -10,6 +10,11 @@ using UnityEngine;
 
 public static class Settings
 {
+    #region DUNGEON BUILD SETTINGS
+        public const int MAX_DUNGEON_REBUILD_ATTEMPTS_FOR_ROOM_GRAPH = 1000;
+        public const int MAX_DUNGEON_BUILD_ATTEMPTS = 10;
+    #endregion DUNGEON BUILD SETTINGS
+    
     #region ROOM SETTINGS
 
     public const int MAXCHILDCORRIDORS = 3;
