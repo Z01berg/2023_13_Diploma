0c4bdab0306d970be493636996fb60ac23bed77b
Juliusz <s23751@pjwstk.edu.pl>
2024-10-21 16:48:25 +0200

    [Update][EnemyId variable added to TimerData class]

--- Assets/Scripts/Round system/TimerData.cs
+++ Assets/Scripts/Round system/TimerData.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
 /**
- * Publiczna clasa TimerData ma za zadanie {get; set} 3 rzeczy:
+ * Publiczna clasa TimerData ma za zadanie {get; set} 4 rzeczy:
  * - Value (znaczenie zegarka)
  * - Tag (Tag z enuma "boss"; "player")
  * - HP (znaczenie HP)
+ * - EnemyID (identyfikator przeciwnika)
  */
 
 public class TimerData
@@ -12,4 +13,5 @@ public class TimerData
     public int Value { get; set; }
     public string Tag { get; set; }
     public GameObject HP { get; set; }
+    public int EnemyId { get; set;}
 }
