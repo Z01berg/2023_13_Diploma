500da90c6563218ac8c8cf2c16b7070f15f57f3b
s22714 <s22714@pjwstk.edu.pl>
2024-11-20 22:09:46 +0100

    Update: Resolution settings work now

---
+++ Assets/AppSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppSettings
+{
+    public int qualitySettingPreference;
+    public int resolutionPreference;
+    public int textureQualityPreference;
+    public int antiAliasingPreference;
+    public bool fullscreenPreference;
+    public float volumePreference;
+}
--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using TMPro;
+using Unity.Mathematics;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -16,17 +23,20 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private Slider _volumeSlider;
     [SerializeField] private float _currentVolume;
     [SerializeField] private Resolution[] _resolutions;
+    [SerializeField] private Toggle _fullscreenToggle;
 
+    [SerializeField] private AppSettings _appSettings;
     private void Start()
     {
+
         _resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         _resolutions = Screen.resolutions;
         int currentResolutionIndex = 0;
-
+        //_resolutions = _resolutions.Distinct().ToArray();
         for (int i = 0; i < _resolutions.Length; i++)
         {
-            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            string option = _resolutions[i].width + " x " + _resolutions[i].height + " " + math.round(_resolutions[i].refreshRateRatio.value) + "hz";
             options.Add(option);
             if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
             {
@@ -34,8 +44,20 @@ public class SettingsManager : MonoBehaviour
             }
         }
 
+        //options = options.Distinct().ToList();
+
         _resolutionDropdown.AddOptions(options);
         _resolutionDropdown.RefreshShownValue();
+
+        var qualNames = QualitySettings.names;
+
+        _qualityDropdown.ClearOptions();
+        _qualityDropdown.AddOptions(qualNames.ToList());
+
+        string[] antiAliasingModes = { "Disabled", "2x MSAA", "4x MSAA", "8x MSAA" };
+        _antiAliasingDropdown.ClearOptions();
+        _antiAliasingDropdown.AddOptions(antiAliasingModes.ToList());
+
         LoadSettings(currentResolutionIndex);
     }
 
@@ -44,6 +66,7 @@ public class SettingsManager : MonoBehaviour
         _audioMixer.SetFloat("Volume", volume);
         _currentVolume = volume;
     }
+
 
     public void SetFullscreen(bool isFullscreen)
     {
@@ -54,6 +77,7 @@ public class SettingsManager : MonoBehaviour
     {
         Resolution resolution = _resolutions[index];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        Application.targetFrameRate = Convert.ToInt32(math.round(resolution.refreshRateRatio.value));
     }
 
     public void SetTextureQuality(int textureIndex)
@@ -68,6 +92,8 @@ public class SettingsManager : MonoBehaviour
     }
     public void SetQuality(int qualityIndex)
     {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        /*
         if (qualityIndex != 6) // if the user is not using 
                                //any of the presets
             QualitySettings.SetQualityLevel(qualityIndex);
@@ -100,78 +126,49 @@ public class SettingsManager : MonoBehaviour
         }
 
         _qualityDropdown.value = qualityIndex;
-
+        */
     }
     public void SaveSettings()
     {
-        PlayerPrefs.SetInt("QualitySettingPreference",
-                   _qualityDropdown.value);
-        PlayerPrefs.SetInt("ResolutionPreference",
-                   _resolutionDropdown.value);
-        PlayerPrefs.SetInt("TextureQualityPreference",
-                   _textureDropdown.value);
-        PlayerPrefs.SetInt("AntiAliasingPreference",
-                   _antiAliasingDropdown.value);
-        PlayerPrefs.SetInt("FullscreenPreference",
-                   Convert.ToInt32(Screen.fullScreen));
-        PlayerPrefs.SetFloat("VolumePreference",
-                   _currentVolume);
+        _appSettings.qualitySettingPreference = _qualityDropdown.value;
+        _appSettings.resolutionPreference = _resolutionDropdown.value;
+        _appSettings.textureQualityPreference = _textureDropdown.value;
+        _appSettings.antiAliasingPreference = _antiAliasingDropdown.value;
+        _appSettings.fullscreenPreference = Screen.fullScreen;
+        _appSettings.volumePreference = _currentVolume;
+
+        string json = JsonUtility.ToJson(_appSettings, true);
+
+        System.IO.File.WriteAllText(Application.streamingAssetsPath + "/AppSettings.txt", json);
+        
     }
 
     public void LoadSettings(int currentResolutionIndex)
     {
-        if (PlayerPrefs.HasKey("QualitySettingPreference"))
-        { 
-            _qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
-        }
-        else
+        if (System.IO.File.Exists(Application.streamingAssetsPath + "/AppSettings.txt"))
         {
-            _qualityDropdown.value = 3;
-        }
+            _appSettings = JsonUtility.FromJson<AppSettings>(System.IO.File.ReadAllText(Application.streamingAssetsPath + "/AppSettings.txt"));
 
-        if (PlayerPrefs.HasKey("ResolutionPreference"))
-        {
-            _resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
         }
         else
         {
-            _resolutionDropdown.value = currentResolutionIndex;
+            _appSettings = new AppSettings
+            {
+                qualitySettingPreference = 5,
+                resolutionPreference = currentResolutionIndex,
+                textureQualityPreference = 0,
+                antiAliasingPreference = 1,
+                fullscreenPreference = true,
+                volumePreference = 0.5f
+            };
         }
+        _resolutionDropdown.value = _appSettings.resolutionPreference;
+        _textureDropdown.value = _appSettings.textureQualityPreference;
+        _antiAliasingDropdown.value = _appSettings.antiAliasingPreference;
+        _volumeSlider.value = _appSettings.volumePreference;
+        _currentVolume = _appSettings.volumePreference;
+        Screen.fullScreen = _appSettings.fullscreenPreference;
+        _fullscreenToggle.isOn = _appSettings.fullscreenPreference;
 
-        if (PlayerPrefs.HasKey("TextureQualityPreference"))
-        {
-            _textureDropdown.value = PlayerPrefs.GetInt("TextureQualityPreference");
-        }
-        else
-        {
-            _textureDropdown.value = 0;
-        }
-
-        if (PlayerPrefs.HasKey("AntiAliasingPreference"))
-        {
-            _antiAliasingDropdown.value = PlayerPrefs.GetInt("AntiAliasingPreference");
-        }
-        else
-        {
-            _antiAliasingDropdown.value = 1;
-        }
-
-        if (PlayerPrefs.HasKey("FullscreenPreference"))
-        {
-            Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
-        }
-        else
-        {
-            Screen.fullScreen = true;
-        }
-
-        if (PlayerPrefs.HasKey("VolumePreference"))
-        {
-            _volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
-        }
-        else
-        {
-            _volumeSlider.value = PlayerPrefs.GetFloat("VolumePreference");
-        }
     }
 }
