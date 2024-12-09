00d54a977c7fd069af38c6dd2c2679a7bab31ee6
Juliusz <s23751@pjwstk.edu.pl>
2024-12-09 14:41:06 +0100

    [Update][CameraProperties.cs] Smoother and a bit more stable camera system, new camera mode
    I smoothed out the camera work, it should be a bit better. I also added a condition for changing the camera mode.

--- Assets/Scripts/Camera/CameraProperties.cs
+++ Assets/Scripts/Camera/CameraProperties.cs
@@ -8,42 +8,129 @@ using UnityEngine;
 
 public class CameraProperties : MonoBehaviour
 {
-    
-    //[SerializeField] private float _zoomOutOnTheRoomValue;
-    //[SerializeField] private float _zoomInOnTheRoomValue;
-    
     public CinemachineVirtualCamera virtualCamera;
     public Transform player;
-    public float zoomSpeed = 2f;
-    public float minZoom = 1f;
-    public float maxZoom = 100f;
+    private float _zoomSpeed = 10f;
+    private float _zoomInertia = 2f;
+    private float _minZoom = 5f;
+    private float _maxZoom = 10f;
+    private float _defaultZoom = 7.5f;
     private float _previousZoom;
-
-
+    private float _targetZoom;
+    private float _cameraPanSpeed = 15f;
+    private float _cameraPanBorderWidth = 5f;
+    private float _cameraPanLimit = 10;
+    private Vector3 _velocity = Vector3.zero;
+    private float _cameraMoveSmoothness = 0.3f;
+    [HideInInspector] public bool isFollowEnabled = true;
+    [HideInInspector] public bool isCameraPanEnabled = false;
+    
     private void Start()
     {
         if (player == null)
         {
-            Debug.LogError("Player obj not connected to the camera");
+            Debug.LogError("Player obj not connected to the camera script");
         }
+
+        if (virtualCamera == null)
+        {
+            Debug.LogError("Cinemachine virtual camera not connected to the camera script");
+        }
+
+        _targetZoom = _defaultZoom;
+        virtualCamera.m_Lens.OrthographicSize = _defaultZoom;
     }
 
     private void Update()
     {
         float zoomInput = Input.GetAxis("Mouse ScrollWheel");
         ZoomCamera(zoomInput);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isCameraPanEnabled = !isCameraPanEnabled;
+            if (isCameraPanEnabled)
+            {
+                virtualCamera.Follow = null;
+            } else if (isFollowEnabled)
+            {
+                virtualCamera.Follow = player.transform;
+            }
+        }
+
+        if (isFollowEnabled && !isCameraPanEnabled && !CombatMode.isPlayerInCombat)
+        {
+            virtualCamera.Follow = player.transform;
+        }
+
+        if (!isFollowEnabled || isCameraPanEnabled)
+        {
+            virtualCamera.Follow = null;
+            PanCamera();
+        }
     }
 
     private void ZoomCamera(float increment)
     {
         float currentZoom = virtualCamera.m_Lens.OrthographicSize;
-        float newZoom = Mathf.Clamp(currentZoom - increment * zoomSpeed, minZoom, maxZoom);
-        virtualCamera.m_Lens.OrthographicSize = newZoom;
+        _targetZoom -= increment * _zoomInertia;
+        _targetZoom = Mathf.Clamp(_targetZoom, _minZoom, _maxZoom);
+        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(currentZoom, _targetZoom, Time.deltaTime * _zoomSpeed);
+    }
+
+    private void PanCamera()
+    {
+        Vector3 cameraPosition = virtualCamera.transform.position;
+        Vector3 playerPosition = player.transform.position;
+
+        if (Input.mousePosition.x >= Screen.width - _cameraPanBorderWidth)
+        {
+            cameraPosition.x += _cameraPanSpeed * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.x <= _cameraPanBorderWidth)
+        {
+            cameraPosition.x -= _cameraPanSpeed * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.y >= Screen.height - _cameraPanBorderWidth)
+        {
+            cameraPosition.y += _cameraPanSpeed * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.y <= _cameraPanBorderWidth)
+        {
+            cameraPosition.y -= _cameraPanSpeed * Time.deltaTime;
+        }
+
+        cameraPosition.x = Mathf.Clamp(
+            cameraPosition.x, 
+            playerPosition.x - _cameraPanLimit, 
+            playerPosition.x + _cameraPanLimit
+            );
+        cameraPosition.y = Mathf.Clamp(
+            cameraPosition.y, 
+            playerPosition.y - _cameraPanLimit, 
+            playerPosition.y + _cameraPanLimit
+            );
+
+        virtualCamera.transform.position = cameraPosition;
     }
 
     public void AdjustCameraToTheRoomSize(Vector2 size, Vector3 centre)
     {
-        virtualCamera.m_Lens.OrthographicSize = MathF.Max(size.x, size.y);
+        float targetZoom = MathF.Max(size.x, size.y);
+        virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(
+            virtualCamera.m_Lens.OrthographicSize,
+            targetZoom,
+            Time.deltaTime * _zoomSpeed);
         virtualCamera.transform.position = new Vector3(centre.x, centre.y, centre.z);
+        
+        Vector3 currentPosition = virtualCamera.transform.position;
+        Vector3 targetPosition = new Vector3(centre.x, centre.y, currentPosition.z);
+        virtualCamera.transform.position = Vector3.Lerp(
+            currentPosition,
+            targetPosition,
+            Time.deltaTime * _cameraMoveSmoothness
+        );
     }
 }
